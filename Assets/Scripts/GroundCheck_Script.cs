d05e4be1052faeebedb5206bba1e70140aa23365
Kyo <c.zheng@mtp.ecolescreatives.com>
2024-05-07 00:54:27 +0200

    Update GroundCheck_Script.cs

--- Assets/Scripts/GroundCheck_Script.cs
+++ Assets/Scripts/GroundCheck_Script.cs
@@ -6,21 +6,6 @@ public class GroundCheck_Script : MonoBehaviour
 {
     public bool isGrounded;
 
-
-    [SerializeField] Player_Script1 player;
-
-    [SerializeField] private Transform LastCheckpoint;
-    [SerializeField] private GameObject playerObject;
-
-    void Update()
-    {
-        if (player.life <= 0)
-        {
-            Die();
-            player.life = player.maxlife;
-        }
-    }
-
     private void OnCollisionEnter2D(Collision2D other) //hit different game object
     {
         if (other.gameObject.CompareTag("Ground")) // collide an object with a tag 
@@ -28,14 +13,7 @@ public class GroundCheck_Script : MonoBehaviour
             isGrounded = true; // not jumping
         }
 
-        if (other.gameObject.CompareTag("Checkpoint"))
-        {
-            Transform CurrentCheckpoint = other.gameObject.transform;
 
-            CheckpointUpdate(CurrentCheckpoint);
-
-            // collision.GetComponent<Checkpoint>().CheckpointFeedback();
-        }
     }
 
     private void OnCollisionExit2D(Collision2D other) //leaving the floor 
@@ -45,15 +23,5 @@ public class GroundCheck_Script : MonoBehaviour
             isGrounded = false; // jumping
         }
 
-    }
-
-    private void CheckpointUpdate(Transform checkpointTransform)
-    {
-        LastCheckpoint = checkpointTransform;
-    }
-
-    private void Die()
-    {
-        playerObject.transform.position = LastCheckpoint.position;
     }
 }
