1cd16d7c833f8b0e34f5c338040d798813c8363d
Kyo <c.zheng@mtp.ecolescreatives.com>
2024-05-21 18:41:06 +0200

    trigger

--- Assets/Trigger.cs
+++ Assets/Trigger.cs
@@ -8,18 +8,44 @@ public class Trigger : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI interText;
 
-    [SerializeField] string[] text;
+    [SerializeField] string[] textgp; // for gamepad
+    [SerializeField] string[] textfr; // for azerty
+    [SerializeField] string[] texten; // for qwerty
+
+    [SerializeField] int times = 0;
+    [SerializeField] int requiredtime;
+
+    void Start()
+    {
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // if collision with player, player gets damage
         if (collision.gameObject.tag == "Player")
         {
-            StartCoroutine(triggerCoroutine());
+            times++;
+            if(times == requiredtime)
+            {
+                if(MainMenu.gamepad)
+                {
+                    StartCoroutine(triggerCoroutine(textgp));
+                }
+                else if(Application.systemLanguage == SystemLanguage.French)
+                {
+                    StartCoroutine(triggerCoroutine(textfr));
+                }
+                else
+                {
+                    StartCoroutine(triggerCoroutine(texten));
+                }
+            }
+            
         }
     }
 
-    IEnumerator triggerCoroutine()
+    IEnumerator triggerCoroutine(string[] text)
     {
         int i = 0;
         while (i < text.Length)
