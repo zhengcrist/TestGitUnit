ee60bfa17c6df00aea9802ab9afd013a8f23f6e0
Kyo <c.zheng@mtp.ecolescreatives.com>
2024-05-26 03:34:38 +0200

    mob life-- and advance

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ChangeScene : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI tmptext;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Return the current Active Scene in order to get the current Scene name.
@@ -19,7 +22,14 @@ public class ChangeScene : MonoBehaviour
             }
             else if (scene.name == "SCN_Game2")
             {
-                SceneManager.LoadScene("SCN_GameFinal");
+                if ((Inventory_Script.MedNum > 0) && (Inventory_Script.OilNum > 0) && (Inventory_Script.ToadNum > 0))
+                {
+                    SceneManager.LoadScene("SCN_GameFinal");
+                }
+                else
+                {
+                    tmptext.text = "I should gather enough ingredients before.";
+                }
             }
         }
     }
